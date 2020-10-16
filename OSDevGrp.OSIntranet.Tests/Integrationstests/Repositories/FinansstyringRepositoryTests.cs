fed09214ca7de4723aa5a4e178974b4bef654c93
Ole Sørensen <mrgottham@gmail.com>
2020-10-16 08:41:07 +0200

    Implementing functionality to export accounts, budget accounts and contact accounts

--- OSDevGrp.OSIntranet.Tests/Integrationstests/Repositories/FinansstyringRepositoryTests.cs
+++ OSDevGrp.OSIntranet.Tests/Integrationstests/Repositories/FinansstyringRepositoryTests.cs
@@ -4,13 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using NUnit.Framework;
+using OSDevGrp.OSIntranet.CommonLibrary.Domain.Adressekartotek;
 using OSDevGrp.OSIntranet.CommonLibrary.Domain.Finansstyring;
 using OSDevGrp.OSIntranet.CommonLibrary.IoC;
 using OSDevGrp.OSIntranet.Domain.Adressekartotek;
 using OSDevGrp.OSIntranet.Domain.Fælles;
-using OSDevGrp.OSIntranet.Repositories.Interfaces;
-using NUnit.Framework;
+using OSDevGrp.OSIntranet.Domain.Interfaces.Adressekartotek;
 using OSDevGrp.OSIntranet.Domain.Interfaces.Fælles;
+using OSDevGrp.OSIntranet.Repositories.Interfaces;
 
 namespace OSDevGrp.OSIntranet.Tests.Integrationstests.Repositories
 {
@@ -123,12 +125,16 @@ namespace OSDevGrp.OSIntranet.Tests.Integrationstests.Repositories
             Assert.That(budgetkontoØvrigeUdgifter.Bogføringslinjer.Count(), Is.EqualTo(bogføringer + 2));
         }
 
-        [Test] 
+        [Test]
         [Ignore("Used for data migration")]
         public void Export()
         {
             IBrevhovedlisteHelper letterHeadHelper = new BrevhovedlisteHelper(_fællesRepository.BrevhovedGetAll());
-            IEnumerable<Regnskab> accountingCollection = _finansstyringRepository.RegnskabslisteGet(letterHeadHelper.GetById);
+            IAdresselisteHelper addressHelper = new AdresselisteHelper(_adresseRepository.AdresseGetAll());
+            IEnumerable<Regnskab> accountingCollection = _finansstyringRepository
+                .RegnskabslisteGet(letterHeadHelper.GetById)
+                .Select(accounting => _finansstyringRepository.RegnskabGet(accounting.Nummer, letterHeadHelper.GetById, addressHelper.GetById))
+                .ToArray();
 
             XmlDocument accountingDocument = new XmlDocument();
             accountingDocument.AppendChild(accountingDocument.CreateXmlDeclaration("1.0", Encoding.UTF8.BodyName, null));
@@ -149,10 +155,145 @@ namespace OSDevGrp.OSIntranet.Tests.Integrationstests.Repositories
                 accountingElement.Attributes.Append(accountingDocument.CreateAttribute("letterHeadNumber"));
                 accountingElement.Attributes["letterHeadNumber"].Value = accounting.Brevhoved.Nummer.ToString();
 
+                foreach (Konto account in accounting.Konti.OfType<Konto>())
+                {
+                    XmlElement accountElement = accountingDocument.CreateElement("Account");
+
+                    accountElement.Attributes.Append(accountingDocument.CreateAttribute("accountNumber"));
+                    accountElement.Attributes["accountNumber"].Value = account.Kontonummer.Trim().ToUpper();
+
+                    accountElement.Attributes.Append(accountingDocument.CreateAttribute("accountName"));
+                    accountElement.Attributes["accountName"].Value = account.Kontonavn.Trim();
+
+                    if (string.IsNullOrWhiteSpace(account.Beskrivelse) == false)
+                    {
+                        accountElement.Attributes.Append(accountingDocument.CreateAttribute("description"));
+                        accountElement.Attributes["description"].Value = account.Beskrivelse.Trim();
+                    }
+
+                    if (string.IsNullOrWhiteSpace(account.Note) == false)
+                    {
+                        accountElement.Attributes.Append(accountingDocument.CreateAttribute("note"));
+                        accountElement.Attributes["note"].Value = account.Note.Trim();
+                    }
+
+                    accountElement.Attributes.Append(accountingDocument.CreateAttribute("accountGroup"));
+                    accountElement.Attributes["accountGroup"].Value = account.Kontogruppe.Nummer.ToString();
+
+                    accountingElement.AppendChild(accountElement);
+                }
+
+                foreach (Budgetkonto budgetAccount in accounting.Konti.OfType<Budgetkonto>())
+                {
+                    XmlElement budgetAccountElement = accountingDocument.CreateElement("BudgetAccount");
+
+                    budgetAccountElement.Attributes.Append(accountingDocument.CreateAttribute("accountNumber"));
+                    budgetAccountElement.Attributes["accountNumber"].Value = budgetAccount.Kontonummer.Trim().ToUpper();
+
+                    budgetAccountElement.Attributes.Append(accountingDocument.CreateAttribute("accountName"));
+                    budgetAccountElement.Attributes["accountName"].Value = budgetAccount.Kontonavn;
+
+                    if (string.IsNullOrWhiteSpace(budgetAccount.Beskrivelse) == false)
+                    {
+                        budgetAccountElement.Attributes.Append(accountingDocument.CreateAttribute("description"));
+                        budgetAccountElement.Attributes["description"].Value = budgetAccount.Beskrivelse.Trim();
+                    }
+
+                    if (string.IsNullOrWhiteSpace(budgetAccount.Note) == false)
+                    {
+                        budgetAccountElement.Attributes.Append(accountingDocument.CreateAttribute("note"));
+                        budgetAccountElement.Attributes["note"].Value = budgetAccount.Note.Trim();
+                    }
+
+                    budgetAccountElement.Attributes.Append(accountingDocument.CreateAttribute("budgetAccountGroup"));
+                    budgetAccountElement.Attributes["budgetAccountGroup"].Value = budgetAccount.Budgetkontogruppe.Nummer.ToString();
+
+                    accountingElement.AppendChild(budgetAccountElement);
+                }
+
+                AdresseBase[] contactAccountCollection = accounting.Konti.OfType<Konto>()
+                    .SelectMany(account => account.Bogføringslinjer)
+                    .Where(postingLine => postingLine.Adresse != null)
+                    .Select(postingLine => postingLine.Adresse.Nummer)
+                    .Distinct()
+                    .Select(number => addressHelper.GetById(number))
+                    .ToArray();
+                foreach (AdresseBase contactAccount in contactAccountCollection)
+                {
+                    XmlElement contactAccountElement = accountingDocument.CreateElement("ContactAccount");
+
+                    contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("accountNumber"));
+                    contactAccountElement.Attributes["accountNumber"].Value = contactAccount.Nummer.ToString();
+
+                    contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("accountName"));
+                    contactAccountElement.Attributes["accountName"].Value = contactAccount.Navn.Trim();
+
+                    if (contactAccount is Person person)
+                    {
+                        if (string.IsNullOrWhiteSpace(person.Mobil) == false && string.IsNullOrWhiteSpace(person.Telefon) == false)
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = person.Telefon.Trim();
+
+                            if (string.CompareOrdinal(person.Mobil, person.Telefon) != 0)
+                            {
+                                contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("secondaryPhone"));
+                                contactAccountElement.Attributes["secondaryPhone"].Value = person.Mobil.Trim();
+                            }
+                        }
+                        else if (string.IsNullOrWhiteSpace(person.Mobil) == false && string.IsNullOrWhiteSpace(person.Telefon))
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = person.Mobil.Trim();
+                        }
+                        else if (string.IsNullOrWhiteSpace(person.Mobil) && string.IsNullOrWhiteSpace(person.Telefon) == false)
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = person.Telefon.Trim();
+                        }
+                    }
+
+                    if (contactAccount is Firma company)
+                    {
+                        if (string.IsNullOrWhiteSpace(company.Telefon1) == false && string.IsNullOrWhiteSpace(company.Telefon2) == false)
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = company.Telefon1.Trim();
+
+                            if (string.CompareOrdinal(company.Telefon1, company.Telefon2) != 0)
+                            {
+                                contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("secondaryPhone"));
+                                contactAccountElement.Attributes["secondaryPhone"].Value = company.Telefon2.Trim();
+                            }
+                        }
+                        else if (string.IsNullOrWhiteSpace(company.Telefon1) == false && string.IsNullOrWhiteSpace(company.Telefon2))
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = company.Telefon1.Trim();
+                        }
+                        else if (string.IsNullOrWhiteSpace(company.Telefon1) && string.IsNullOrWhiteSpace(company.Telefon2) == false)
+                        {
+                            contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("primaryPhone"));
+                            contactAccountElement.Attributes["primaryPhone"].Value = company.Telefon2.Trim();
+                        }
+                    }
+
+                    if (string.IsNullOrWhiteSpace(contactAccount.Mailadresse) == false)
+                    {
+                        contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("mailAddress"));
+                        contactAccountElement.Attributes["mailAddress"].Value = contactAccount.Mailadresse.Trim();
+                    }
+
+                    contactAccountElement.Attributes.Append(accountingDocument.CreateAttribute("paymentTermNumber"));
+                    contactAccountElement.Attributes["paymentTermNumber"].Value = contactAccount.Betalingsbetingelse.Nummer.ToString();
+
+                    accountingElement.AppendChild(contactAccountElement);
+                }
+
                 accountingCollectionElement.AppendChild(accountingElement);
             }
 
-            using (FileStream fileStream = new FileStream(@"Accountings.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            using (FileStream fileStream = new FileStream("Accountings.xml", FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 XmlWriterSettings settings = new XmlWriterSettings
                 {
